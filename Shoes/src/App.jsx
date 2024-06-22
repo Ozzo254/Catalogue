4cde3f39750147f8f14a7ff1971025858b6d633c
Ozzo_254 <veetruth99@gmail.com>
2024-06-22 18:07:33 +0100

    init commit

---
+++ Shoes/src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./Layouts/NavBar/Nav";
+import Products from "./Layouts/Products/Products";
+import Recommended from "./Layouts/Recommended/Recommended";
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Recommended />
+      <Products />
+    </>
+  )
+}
+
+export default App
